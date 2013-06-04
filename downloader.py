b219c32fa0ec24156f2e1799e41e450891a6fc39
crccheck <c@crccheck.com>
2013-06-04 04:04:47 +0000

    add ability to save to a subdirectory

--- downloader.py
+++ downloader.py
@@ -11,11 +11,22 @@ import requests
 
 # http://zootool.com/api/docs/users
 url_endpoint = 'http://zootool.com/api/users/items/'
+download_base = 'download'
 
 
 def get_filename_from_url(url):
     path = urlparse.urlparse(url).path
     return os.path.split(path)[-1]
+
+
+def download(url, save_path):
+    """Because urlretrieve is a wimp."""
+    with open(save_path, 'wb') as fh:
+        response = requests.get(url, stream=True)
+        for block in response.iter_content(1024):
+            if not block:
+                break
+            fh.write(block)
 
 
 def main(username):
@@ -26,15 +37,17 @@ def main(username):
     )
     response = requests.get(url_endpoint, params=params)
     data = response.json()
+    if not os.path.isdir(download_base):
+        os.mkdir(download_base)
     # TODO de-paginate
     for item in data:
         url = item['url']
         filename = get_filename_from_url(url)
         assert filename
-        save_path = 'download/{}'.format(filename)  # TODO sort into tags
+        save_path = os.path.join(download_base, filename)  # TODO sort into tags
         if not os.path.isfile(save_path):
             logger.info('Downloading {} -> {}'.format(url, save_path))
-            urllib.urlretrieve(url, filename)
+            download(url, save_path)
         else:
             logger.debug('File already exists: {}'.format(save_path))
 
